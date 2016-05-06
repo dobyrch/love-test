1f04a66786d3e97738c1ea97d829e4277747577d
Douglas Christman <DouglasChristman@gmail.com>
2016-05-05 21:20:28 -0400

    Fix movement speed
    Increase speed and animation frequency;
    reduce diagonal movement speed

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 scale = 4
-player = { x = 70*scale, y = 70*scale, speed = 40*scale, img = nil, quad = nil}
+player = { x = 70*scale, y = 70*scale, speed = 60*scale, img = nil, quad = nil}
 up_step1 = nil
 up_step2 = nil
 right_stand = nil
@@ -27,19 +27,21 @@ end
 dtotal = 0
 function love.update(dt)
 	dtotal = dtotal + dt
-	if dtotal >= 0.5 then
-		dtotal = dtotal - 0.5
+	if dtotal >= 0.25 then
+		dtotal = dtotal - 0.25
 	end
 
 	if love.keyboard.isDown('q') then
 		love.event.push('quit')
 	end
 
-	x, y, width, height = player.quad:getViewport()
+	_, _, width, height = player.quad:getViewport()
+	dx, dy = 0, 0
+	oldquad = player.quad
 	if love.keyboard.isDown('left','s') then
 		if player.x > 0 then
-			player.x = player.x - (player.speed*dt)
-			if dtotal < 0.25 then
+			dx = -player.speed * dt
+			if dtotal < 0.125 then
 				player.quad = left_stand
 			else
 				player.quad = left_step
@@ -48,8 +50,8 @@ function love.update(dt)
 	end
 	if love.keyboard.isDown('right','f') then
 		if player.x < (love.graphics.getWidth() - width*scale) then
-			player.x = player.x + (player.speed*dt)
-			if dtotal < 0.25 then
+			dx = player.speed * dt
+			if dtotal < 0.125 then
 				player.quad = right_stand
 			else
 				player.quad = right_step
@@ -58,8 +60,8 @@ function love.update(dt)
 	end
 	if love.keyboard.isDown('down','d') then
 		if player.y < (love.graphics.getHeight() - height*scale) then
-			player.y = player.y + (player.speed*dt)
-			if dtotal < 0.25 then
+			dy = player.speed * dt
+			if dtotal < 0.125 then
 				player.quad = down_step1
 			else
 				player.quad = down_step2
@@ -68,14 +70,26 @@ function love.update(dt)
 	end
 	if love.keyboard.isDown('up','e') then
 		if player.y > 0 then
-			player.y = player.y - (player.speed*dt)
-			if dtotal < 0.25 then
+			dy = -player.speed * dt
+			if dtotal < 0.125 then
 				player.quad = up_step1
 			else
 				player.quad = up_step2
 			end
 		end
 	end
+
+	if dx == dy == 0 then
+		player.quad = oldquad
+	end
+
+	if dx ~= 0 and dy ~= 0 then
+		dx = dx / math.sqrt(2)
+		dy = dy / math.sqrt(2)
+	end
+
+	player.x = player.x + dx
+	player.y = player.y + dy
 end
 
 
