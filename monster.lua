b7784048b9dc800b991d6915a1f21768e49540c5
Douglas Christman <DouglasChristman@gmail.com>
2016-05-14 21:14:47 -0400

    Fix sprite positioning

--- monster.lua
+++ monster.lua
@@ -21,14 +21,14 @@ function Monster:new()
 
 	self.img = love.graphics.newImage('assets/octorok.png')
 	self.img:setFilter('linear', 'nearest')
-	self.quads[1] = love.graphics.newQuad(1, 0, 16, 16, self.img:getDimensions())
-	self.quads[2] = love.graphics.newQuad(18, 0, 16, 16, self.img:getDimensions())
-	self.quads[3] = love.graphics.newQuad(36, 0, 16, 16, self.img:getDimensions())
-	self.quads[4] = love.graphics.newQuad(54, 0, 16, 16, self.img:getDimensions())
-	self.quads[5] = love.graphics.newQuad(72, 0, 16, 16, self.img:getDimensions())
-	self.quads[6] = love.graphics.newQuad(90, 0, 16, 16, self.img:getDimensions())
-	self.quads[7] = love.graphics.newQuad(108, 0, 16, 16, self.img:getDimensions())
-	self.quads[8] = love.graphics.newQuad(125, 0, 16, 16, self.img:getDimensions())
+	self.quads[1] = love.graphics.newQuad(1, 0, 16, 17, self.img:getDimensions())
+	self.quads[2] = love.graphics.newQuad(18, 0, 16, 17, self.img:getDimensions())
+	self.quads[3] = love.graphics.newQuad(36, 0, 16, 17, self.img:getDimensions())
+	self.quads[4] = love.graphics.newQuad(54, 0, 16, 17, self.img:getDimensions())
+	self.quads[5] = love.graphics.newQuad(72, 0, 16, 17, self.img:getDimensions())
+	self.quads[6] = love.graphics.newQuad(90, 0, 16, 17, self.img:getDimensions())
+	self.quads[7] = love.graphics.newQuad(108, 0, 16, 17, self.img:getDimensions())
+	self.quads[8] = love.graphics.newQuad(125, 0, 16, 17, self.img:getDimensions())
 	self.q = 1
 	self.steps = 0
 
