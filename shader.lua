2fee044a3b0f43724477d01f33dcd765ea4d5cf1
Douglas Christman <DouglasChristman@gmail.com>
2016-05-17 18:53:47 -0400

    Define flash duration in one place

--- shader.lua
+++ shader.lua
@@ -1,16 +1,17 @@
 local shader = {}
 
 shader.damaged = love.graphics.newShader [[
+	#define FLASH_DUR 8.0/60.0
 	extern number time;
 
 	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
 		vec4 pixel = Texel(texture, texture_coords);
 		number flash = time;
 
-		while (flash >= 8.0 / 60.0)
-			flash -= 8.0 / 60.0;
+		while (flash >= FLASH_DUR)
+			flash -= FLASH_DUR;
 
-		if (flash > 4.0 / 60.0)
+		if (flash > FLASH_DUR/2)
 			return pixel;
 
 		if (pixel.r + pixel.g + pixel.b > 1.5) {
